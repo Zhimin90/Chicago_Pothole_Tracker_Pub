d2cfd29c8622b9151e1ba8d2ae607592c194764e
Zhimin90 <zhiminzou@gmail.com>
2020-03-18 23:33:37 -0500

    updated geodjango model file

--- geodjango/world/views.py
+++ geodjango/world/views.py
@@ -6,6 +6,9 @@ from world.models import WorldBorder, T_Potholes, Density_Map
 from django.contrib.gis.geos import GEOSGeometry, Point, Polygon
 from datetime import datetime
 import ast
+import tensorflow as tf
+#import geopandas as gpd
+#from tensorflow.keras.models import Sequential
 
 # Create your views here.
 def get_density_map(request):
@@ -20,7 +23,11 @@ def get_density_map(request):
     #bound = Polygon.from_bbox((-87.65797272125538,  41.95381335885449, -87.63888550483443, 41.97115012250981))
     query = Density_Map.objects.filter(poly_coordinate__bboverlaps = bound).filter(end_date__date__gte = datetime(2018, 11, 10), )
     #query = Density_Map.objects.filter(end_date__date__gte = datetime(2018, 11, 10), )
-    geojson = serialize('geojson', query,  fields=('density','start_date','end_date', 'poly_coordinate'))
+    print("Serializing")
+    geojson = serialize('geojson', query, geometry_field='poly_coordinate', fields=('density','start_date','end_date'))
+    print("Done Serializing")
     return(HttpResponse(geojson))
 
+def load_data_frame(request):
+    print("loading DataFrame")
 

9bf6e1b2c2025d56cd72c0653912ab4c1a96921e
Zhimin90 <zhiminzou@gmail.com>
2020-03-24 12:48:40 -0500

    Redeploy after changing Procfile

--- geodjango/geodjango/wsgi.py
+++ geodjango/geodjango/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                "..")))
+                                                
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geodjango.settings")

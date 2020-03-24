5f9ba8458701f2dcf5c3adf7f848c81be8b3895a
Zhimin90 <zhiminzou@gmail.com>
2020-03-24 12:56:39 -0500

    Redeploy after changing Procfile

--- geodjango/geodjango/wsgi.py
+++ geodjango/geodjango/wsgi.py
@@ -11,6 +11,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
                                                 "..")))
+import world
                                                 
 from django.core.wsgi import get_wsgi_application
 
